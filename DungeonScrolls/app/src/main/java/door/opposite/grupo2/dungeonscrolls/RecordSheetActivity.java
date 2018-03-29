a59ac77340d8a067fbeec6254bcd981a793bb494
drayton80 <drayton80@hotmail.com>
2018-03-29 19:24:30 -0300

    v0.1.6 - Activity da Ficha Adicionada

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/RecordSheetActivity.java
@@ -0,0 +1,13 @@
+package door.opposite.grupo2.dungeonscrolls;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RecordSheetActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_sheet);
+    }
+}
