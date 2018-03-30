4e44f89901c0e0f6971afb027411f1e5fd45e894
drayton80 <drayton80@hotmail.com>
2018-03-30 20:27:51 -0300

    v0.2.2 - Modelos das Telas Completos

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/SheetBattleInformationActivityDF.java
@@ -0,0 +1,13 @@
+package door.opposite.grupo2.dungeonscrolls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SheetBattleInformationActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_battle_information_df);
+    }
+}
