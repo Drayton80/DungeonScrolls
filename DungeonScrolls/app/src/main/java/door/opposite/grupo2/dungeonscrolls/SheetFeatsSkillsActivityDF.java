1cf2e7681fab7b3e20571f3f849c87552bd7d635
drayton80 <drayton80@hotmail.com>
2018-03-29 23:36:43 -0300

    v0.1.8 - Dialog Fragment Feats and Skills Incompleto

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/SheetFeatsSkillsActivityDF.java
@@ -0,0 +1,13 @@
+package door.opposite.grupo2.dungeonscrolls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SheetFeatsSkillsActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_feats_skills_df);
+    }
+}
