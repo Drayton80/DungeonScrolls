25dd0ea3def798967c3c6239ab3aed753e44705a
drayton80 <drayton80@hotmail.com>
2018-03-30 14:27:57 -0300

    v2.0.0 - Equipment Dialog Fragment Complete

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/SheetEquipmentItensActivityDF.java
@@ -0,0 +1,13 @@
+package door.opposite.grupo2.dungeonscrolls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SheetEquipmentItensActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_equipment_itens_df);
+    }
+}
