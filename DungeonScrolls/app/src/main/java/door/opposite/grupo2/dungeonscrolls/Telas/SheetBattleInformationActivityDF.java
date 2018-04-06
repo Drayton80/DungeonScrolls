46845e2893ed816fb09722bcf181b138345a9c2c
Drayton80 <drayton80@hotmail.com>
2018-04-06 12:43:05 -0300

    v0.3.0 - Adicionado Special Properties DP (Completo)  e Room Creation (Incompleto)

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/Telas/SheetBattleInformationActivityDF.java
@@ -0,0 +1,15 @@
+package door.opposite.grupo2.dungeonscrolls.Telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import door.opposite.grupo2.dungeonscrolls.R;
+
+public class SheetBattleInformationActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_battle_information_df);
+    }
+}
---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/Telas/SheetEquipmentItensActivityDF.java
@@ -0,0 +1,15 @@
+package door.opposite.grupo2.dungeonscrolls.Telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import door.opposite.grupo2.dungeonscrolls.R;
+
+public class SheetEquipmentItensActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_equipment_itens_df);
+    }
+}
