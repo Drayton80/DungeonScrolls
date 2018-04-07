999a4f8457571c68d79cc2bf2668700f63bb7e37
Drayton80 <drayton80@hotmail.com>
2018-04-07 11:20:24 -0300

    v0.3.1 - Adicionado Magic Information DP (Completo)

---
+++ DungeonScrolls/app/src/main/java/door/opposite/grupo2/dungeonscrolls/Telas/SheetMagicInformationActivityDF.java
@@ -0,0 +1,15 @@
+package door.opposite.grupo2.dungeonscrolls.Telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import door.opposite.grupo2.dungeonscrolls.R;
+
+public class SheetMagicInformationActivityDF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheet_magic_information_df);
+    }
+}
